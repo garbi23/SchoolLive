f8b194ed05dc0c8b90b15af70b15e77526d2fe1f
태운 최 <garbi23@naver.com>
2019-05-30 03:19:01 +0900

    SchoolLive

--- SchoolLive/SchoolLive/Form1.Designer.cs
+++ SchoolLive/SchoolLive/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.ClientSize = new System.Drawing.Size(1920, 1080);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.Text = "Form1";
